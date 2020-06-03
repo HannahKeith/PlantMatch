51316b77545459679b4f472739f71973b19b8110
Hannah Keith <hannahkeith@Hannahs-Air.attlocal.net>
2020-06-03 13:41:45 -0400

    added schema

---
+++ models/plants.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const plantSchema = new mongoose.Schema({
+  affirmation: {type: String, required: true},
+  housePlants:
+        [  {name: {type: String, required: true},
+          chakra: {type: String, required: true},
+          tradition: {type: String, required: true}}],
+
+  gardenPlants: {
+        [  {name: {type: String, required: true},
+        chakra: {type: String, required: true},
+        tradition: {type: String, required: true}}]
+});
+
+
+const Affirmation = mongoose.model('affirmation', plantSchema)
+
+module.exports = Affirmation
